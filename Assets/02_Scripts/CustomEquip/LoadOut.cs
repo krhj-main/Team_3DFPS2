6f874a35af47b477ea348bb9e757b4cc3997e2be
BeomDDak <kbs6320@gmail.com>
2024-08-23 02:46:22 +0900

    240823 범수
    무기장착 UI 제작 완료
    남은 것 : 카메라 이동
    캐릭터 무기 들기
    무기 저장해서 나가기

--- Assets/02_Scripts/CustomEquip/LoadOut.cs
+++ Assets/02_Scripts/CustomEquip/LoadOut.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+#region 무기 설명 텍스트 변경관련 클래스
+[System.Serializable]
+public class InfomationText
+{
+    public TextMeshProUGUI typeText;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI infoText;
+
+    public void ChangeText(string type, string name, string info)
+    {
+        typeText.text = type;
+        nameText.text = name;
+        infoText.text = info;
+    }
+}
+#endregion
+
 public class LoadOut : MonoBehaviour
 {
+    #region 패널 선택 및 설명 텍스트
+    [Header("패널 선택 및 설명 텍스트")]
+
+    [Tooltip("무기 선택 패널")]
     public GameObject[] weaponSelectPanel;
 
-    [Space(5)]
-    public Image[] equipWeaponImage; // 장착한 무기
-    public Sprite[] mainWeaponImage;  // 무기 이미지
-    public Button[] selectMainWeaponBtn;
-    int openMainWeaponIndex;
+    [Tooltip("주무기 설명 텍스트")]
+    public InfomationText mainWeaponInfoText;
 
-    // 주무기 이벤트 트리거
-    public TextMeshProUGUI mainType;
-    public TextMeshProUGUI mainName;
-    public TextMeshProUGUI mainInfo;
+    [Tooltip("투척무기 설명 텍스트")]
+    public InfomationText throwingWeaponInfoText;
+    #endregion
+
+    #region 주무기 변수
+    [Space(5)]
+    [Header("주무기 세팅 부분")]
+
+    [Tooltip("장착한 무기")]
+    public Image[] equipWeaponImage;  
+
+    [Tooltip("무기 이미지")]
+    public Sprite[] mainWeaponImage; 
+
+    [Tooltip("주무기 선택 버튼")]
+    public Button[] selectMainWeaponBtn;   
+
+    [Tooltip("주무기 장착칸")]
+    int equipMainWeaponIndex; 
+    public List <int> equipMainWeaponList = new List<int>();
+    #endregion
 
     #region 투척무기 변수
     [Space(5)]
@@ -40,58 +75,74 @@ public class LoadOut : MonoBehaviour
 
     int getThrowing;
     int maxGetThrowing = 5;
-
-    // 투척무기 이벤트 트리거
-    public TextMeshProUGUI throwType;
-    public TextMeshProUGUI throwName;
-    public TextMeshProUGUI throwInfo;
     #endregion
 
-    // 무기 선택 패널 오픈 함수 ( 직접 연결 )
+    #region Awake
+    SelectEquip selectEquip;
+
+    private void Awake()
+    {
+        selectEquip = GetComponent<SelectEquip>();
+    }
+    #endregion
+
+    #region 무기 선택 패널 오픈
     public void OpenSelectPanel(int _index)
     {
-        weaponSelectPanel[_index].SetActive(true);
-        openMainWeaponIndex = _index;
+        selectEquip.ClosePanel();                           // 켜져 있는 패널 있으면 스택에서 빼고 꺼주기
+        selectEquip.PushPanel(weaponSelectPanel[_index]);   // 패널 스택에 넣고 켜주기
+        equipMainWeaponIndex = _index;                      // 주무기 패널 열면 슬롯 확인
+        UpdateSelectWeaponButtonColor();                    // 주무기 패널 열면 색깔 업데이트
     }
+    #endregion
 
     #region 주무기 세팅
     public void GetMainWeapon(int _index)
     {
-        equipWeaponImage[openMainWeaponIndex].sprite = mainWeaponImage[_index];
-        weaponSelectPanel[openMainWeaponIndex].SetActive(false);
+        // 이미 장착된 무기를 선택하면 무시
+        if (equipMainWeaponList.Contains(_index))
+        {
+            return;
+        }
 
-        ColorBlock _colorBlock = selectMainWeaponBtn[_index].colors;
-        _colorBlock.highlightedColor = Color.red;
-        selectMainWeaponBtn[_index].colors = _colorBlock;
+        equipMainWeaponList[equipMainWeaponIndex] = _index;                             // 선택한 무기를 해당 슬롯에 장착
+        equipWeaponImage[equipMainWeaponIndex].sprite = mainWeaponImage[_index];        // 무기 이미지 업데이트
+        selectEquip.ClosePanel();                                                       // 패널 꺼주기
     }
 
+    // 색깔 업데이트
+    void UpdateSelectWeaponButtonColor()
+    {
+        for (int i = 0; i < selectMainWeaponBtn.Length; i++)
+        {
+            ColorBlock _colorBlock = selectMainWeaponBtn[i].colors;
+
+            _colorBlock.highlightedColor = equipMainWeaponList.Contains(i) ? Color.red : Color.green;
+
+            selectMainWeaponBtn[i].colors = _colorBlock;
+        }
+    }
 
     // ==== 이벤트 트리거 ====
-    void ChangeMainText(string type, string name, string info)
-    {
-        mainType.text = type;
-        mainName.text = name;
-        mainInfo.text = info;
-    }
 
     public void MainChangeText1()
     {
-        ChangeMainText("돌격소총", "M4A1", "카빈형 돌격소총\n모든 개선점이 총합된\n완전자동발사형");
+        mainWeaponInfoText.ChangeText("돌격소총", "M4A1", "카빈형 돌격소총\n모든 개선점이 총합된\n완전자동발사형");
     }
 
     public void MainChangeText2()
     {
-        ChangeMainText("산탄총", "M870", "펌프액션 산탄총\n산탄총 앞에서는\n모두가 평등하다");
+        mainWeaponInfoText.ChangeText("산탄총", "M870", "펌프액션 산탄총\n산탄총 앞에서는\n모두가 평등하다");
     }
 
     public void MainChangeText3()
     {
-        ChangeMainText("저격소총", "M24", "볼트액션 저격소총\n강력한 파괴력을 자랑하지만\n조준을 하지 않으면 명중률이 낮다");
+        mainWeaponInfoText.ChangeText("저격소총", "M24", "볼트액션 저격소총\n강력한 파괴력을 자랑하지만\n조준을 하지 않으면 명중률이 낮다");
     }
 
     public void MainChangeText4()
     {
-        ChangeMainText("권총", "HK45", "자동권총\n살상력은 낮지만 은밀하고\n라이트를 사용할 수 있다");
+        mainWeaponInfoText.ChangeText("권총", "HK45", "자동권총\n살상력은 낮지만 은밀하고\n라이트를 사용할 수 있다");
     }
     #endregion
 
@@ -147,26 +198,19 @@ public class LoadOut : MonoBehaviour
 
     // ==== 이벤트 트리거 ====
 
-    void ChangeThrowText(string type, string name, string info)
-    {
-        throwType.text = type;
-        throwName.text = name;
-        throwInfo.text = info;
-    }
-
     public void ThrowChangeText1()
     {
-        ChangeThrowText("수류탄", "MK.2", "나치 놈들과 쪽바리들에게\n강력한 폭발력을 자랑한다");
+        throwingWeaponInfoText.ChangeText("수류탄", "MK.2", "나치 놈들과 쪽바리들에게\n강력한 폭발력을 자랑한다");
     }
 
     public void ThrowChangeText2()
     {
-        ChangeThrowText("연막탄", "M18", "적의 시야를 차단하여\n아군의 회피율을 증가시킨다");
+        throwingWeaponInfoText.ChangeText("연막탄", "M18", "적의 시야를 차단하여\n아군의 회피율을 증가시킨다");
     }
 
     public void ThrowChangeText3()
     {
-        ChangeThrowText("섬광탄", "M84", "직접적인 살상력 없이\n적을 무력화할 수 있다");
+        throwingWeaponInfoText.ChangeText("섬광탄", "M84", "직접적인 살상력 없이\n적을 무력화할 수 있다");
     }
     #endregion
 }
