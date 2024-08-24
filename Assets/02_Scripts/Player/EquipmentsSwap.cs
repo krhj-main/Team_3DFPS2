26103cef15016f5cac7c37dcddec2ee4b839fc58
krhj-main <dygks6666@gmail.com>
2024-08-24 10:24:54 +0900

    240824 준택 자습실부팅

--- Assets/02_Scripts/Player/EquipmentsSwap.cs
+++ Assets/02_Scripts/Player/EquipmentsSwap.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
+
 
 public class EquipmentsSwap : MonoBehaviour
 {
