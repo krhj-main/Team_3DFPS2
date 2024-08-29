61b13d9fc09252ce801ab92af35cfa70fa1653d1
BeomDDak <kbs6320@gmail.com>
2024-08-29 16:28:00 +0900

    240829 범수
    데드 패널 UI 완성

--- Assets/02_Scripts/etc/DeadPanelButton.cs
+++ Assets/02_Scripts/etc/DeadPanelButton.cs
@@ -5,15 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class DeadPanelButton : MonoBehaviour
 {
+    public GameObject[] deadPanelUI;
+    public GameObject charcter;
+    PlayerStateList pState;
+
+    private void Awake()
+    {
+        pState = charcter.GetComponent<PlayerStateList>();
+        
+    }
+
     public void RestartMission()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        offUI();
     }
-
 
     public void GoToLobby()
     {
         SceneManager.LoadScene("Lobby");
+        offUI(); 
+    }
+
+    void offUI()
+    {
+        foreach(GameObject go in deadPanelUI)
+        {
+            go.SetActive(false);
+        }
+        PlayerController.Instance.pHP = 100;
+        pState.isDead = false;
+        Time.timeScale = 1;
     }
 
 }
