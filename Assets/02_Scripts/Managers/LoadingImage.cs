aafe4b475bc43f0fb630fc7ca4577c78a8e1bb6d
krhj-main <dygks6666@gmail.com>
2024-08-30 01:31:58 +0900

    240830 준택 ver 0.2.76
    이미지는 추가했음, 어떻게 스크롤링할지 구상중.
    이미지 추가된 방식도 다른 방식이 있지 않을까 생각은 하는데 일단 구현하기로함..

--- Assets/02_Scripts/Managers/LoadingImage.cs
+++ Assets/02_Scripts/Managers/LoadingImage.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
 
+
+
+/// <summary>
+/// 로딩 시 UI 이미지 띄울 것 받아서 배열로 저장 후, 씬 별로 혹은 랜덤으로 출력시킬 것
+/// UI 이미지와 내용으로 적힐 시나리오 텍스트도 포함
+/// </summary>
 public class LoadingImage : MonoBehaviour
 {
-   
+    [SerializeField] Sprite[] loadingImage;
+    [TextArea]
+    [SerializeField] string[] loadingSceanario;
+
+    TMP_Text text;
+    public Image backgroundImage;
+
+    private void Start()
+    {
+        
+        
+        backgroundImage.sprite = loadingImage[0];
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
 }
