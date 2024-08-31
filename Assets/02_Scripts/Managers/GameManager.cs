4c1fb2978a45a92963b7c534be93947e83640d05
YoungJae970304 <sksdl1643020@gmail.com>
2024-08-31 14:36:50 +0900

    문제해결4
    미션 선택창에서 2번째꺼 활성화되어있는거 수정

--- Assets/02_Scripts/Managers/GameManager.cs
+++ Assets/02_Scripts/Managers/GameManager.cs
@@ -163,7 +163,7 @@ public class GameManager : Singleton<GameManager>
 
     #region 로비 미션 UI 관련 ( 미션선택, 움직임 제한 )
 
-    public int canMissionChoice = 1;        // 게임 클리어 시 미션 목록에서 다음 미션 선택 가능하게 해줌
+    public int canMissionChoice = 0;        // 게임 클리어 시 미션 목록에서 다음 미션 선택 가능하게 해줌
     public bool openUI = false;             // UI 열리면 움직임 제한
     public int selectSceneNum = 0;
 
