e8ffba5007395ca7ee351738d31c17a9732d7a35
YoungJae970304 <sksdl1643020@gmail.com>
2024-08-31 14:19:16 +0900

    문제 해결 3
    NPC, ENEMY Ik 수정

--- Assets/02_Scripts/Animation/AnimIK.cs
+++ Assets/02_Scripts/Animation/AnimIK.cs
@@ -1,42 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimIK : MonoBehaviour
 {
-
     // 총 위치 변수
     public Transform gunPivot;
     public Transform leftHand;
     public Transform rightHand;
-    public Vector3 Offset;
-    public bool trigger; 
+
+    public float lookOffset = 1.5f;
+
     Animator anim;
-    Transform chest;
+    Transform parentsOb;
+
     private void Awake()
     {
-        Offset = new Vector3(-34.3f, 31.4f, -60.40f);
         anim = GetComponent<Animator>();
-        if (anim)
-
-            chest = anim.GetBoneTransform(HumanBodyBones.UpperChest); // 해당 본의 transform 가져오기
-    }
-    private void LateUpdate()
-    {
-
-        //Offset= chest.localRotation.eulerAngles;
-        //Debug.Log(chest.localRotation.eulerAngles);
-        if (trigger) {
-            chest.LookAt(PlayerController.Instance.waist.position);
-            chest.localRotation = Quaternion.Euler(Offset+ chest.localRotation.eulerAngles);
-            Debug.Log(chest.localRotation.eulerAngles);
-        }
-
-    }
-
-    void test() { 
-    
-    
+        parentsOb = GetComponentInParent<Transform>();
     }
 
     // 애니메이터의 IK 갱신
@@ -44,24 +26,28 @@ public class AnimIK : MonoBehaviour
     {
         // 총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
         //gunPivot.position = anim.GetIKHintPosition(AvatarIKHint.RightElbow);
-
-        anim.SetLookAtPosition(PlayerController.Instance.transform.position);
-        anim.SetLookAtWeight(0.3f);
-
-        // IK를 사용하여 왼손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
-        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
-        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
-
-        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.position);
-        anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.rotation);
-
         
-        // IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
-        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
-        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+        anim.SetLookAtPosition(PlayerController.Instance.transform.position + new Vector3(0, lookOffset, 0));
+        anim.SetLookAtWeight(1f);
 
-        anim.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
-        anim.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
-        
+        if (leftHand != null)
+        {
+            // IK를 사용하여 왼손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.position);
+            anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.rotation);
+        }
+
+        if (rightHand != null)
+        {
+            // IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+            anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+            anim.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+            anim.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
+        }
     }
 }
