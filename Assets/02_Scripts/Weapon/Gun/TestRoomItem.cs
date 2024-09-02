62a24b11fd666e0e061a1c6c33459ed56e2afd3e
gray-spade <dltkdwns98@naver.com>
2024-09-02 12:49:21 +0900

    사격장 아이템

---
+++ Assets/02_Scripts/Weapon/Gun/TestRoomItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestRoomItem : MonoBehaviour,Interactable
+{
+    GameObject weaponPrefab;
+    public void Interaction(GameObject target)
+    {
+        MainWeapon weapon = Instantiate(weaponPrefab).GetComponent<MainWeapon>();
+        weapon.Interaction(target);
+    }
+}
