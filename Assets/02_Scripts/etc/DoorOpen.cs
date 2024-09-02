7790d8c47e3baa4d41e5ec0a05499c0d1cb41735
krhj-main <dygks6666@gmail.com>
2024-09-02 15:36:30 +0900

    240902 준택 ver 0.2.98
    문 열고 닫기

--- Assets/02_Scripts/etc/DoorOpen.cs
+++ Assets/02_Scripts/etc/DoorOpen.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorOpen : MonoBehaviour, Interactable
 {
     bool doorOpen = false;
     Animator anim;
-    public void Interaction(GameObject target)
-    {
-        doorOpen = !doorOpen;
-        anim.SetBool("DoorOpen", doorOpen);
-    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        anim.GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+    }
+    
+
+    public void Interaction(GameObject target)
+    {
+        Debug.Log($"문 {doorOpen}");
+        doorOpen = !doorOpen;
+        anim.SetBool("DoorOpen", doorOpen);
     }
 
     // Update is called once per frame
