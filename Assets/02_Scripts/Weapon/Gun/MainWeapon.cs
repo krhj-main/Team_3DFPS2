628e45d89a23e0a5c825c743fafab5d56d28d046
gray-spade <dltkdwns98@naver.com>
2024-09-06 16:58:21 +0900

    샷건 재장전 오류 해제

--- Assets/02_Scripts/Weapon/Gun/MainWeapon.cs
+++ Assets/02_Scripts/Weapon/Gun/MainWeapon.cs
@@ -399,6 +399,8 @@ public class MainWeapon : MonoBehaviour, Interactable, IEquipMent
     {
         loadedAmmo++;
         remainAmmo--;
+        canShoot = true;
+
     }
 
     public void ReloadCheck()
